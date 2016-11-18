8c69dec81a5dfb1e9ba77c436c1b05414160bf52
pesaher <pesaher@gmail.com>
2016-11-18 20:34:08 +0100

    Update characterReader.js

--- javascript/characterReader.js
+++ javascript/characterReader.js
@@ -1,10 +1,5 @@
 var request = new XMLHttpRequest();
-var fortnights;
 request.open("GET", "fortnights.json", false);
-request.send(null);
-request.onreadystatechange = function() {
-  if ( request.readyState === 4 && request.status === 200 ) {
-    var fortnights = JSON.parse(request.responseText);
-    alert (fortnights.fortnights[0].name);
-  }
-}
+request.send(null)
+var fortnights = JSON.parse(request.responseText);
+alert (fortnights.fortnights[0]);
